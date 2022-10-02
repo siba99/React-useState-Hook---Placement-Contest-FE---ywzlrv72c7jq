629adab1d35f0c6231e938780ae780abb67d373b
Siba prasad sahoo <96854388+siba99@users.noreply.github.com>
2022-10-02 15:42:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
 
 import '../styles/App.css';
+
 import React, { useState } from 'react';
 
 export default function App() {
- //code here 
+ //code h
 
- 
+ const[data,setData]=useState(" ");
+ const [print,setPrint]=useState(" ");
+
+const buttonClick=()=>{
+     setData((oldItems)=>{
+      return[...oldItems,print];
+     });
+     setPrint("");
+}
+const changeInput=(e)=>{
+  setPrint(e.target.value);
+}
+
+//  function getValue(e){
+//    setData(e.target.value)
+//  }
   return (
+    
     <div>
-  <input id='input' value={inputValue} onChange={changeInput} />
-  
-      <p id='intro'>Concated String</p>
-      <p id='text'>{text} </p>
-      <button id='button' onClick={buttonClick}>
-        Click me
-      </button>
+    <input id='input' type="text" value={print}  onChange={changeInput}/>
+     {/* {
+      print?
+      <h1>{data}</h1>:
+      null
+    }  */}
+    <p id="intro">Concated String</p>
+    <p id="text">{data}</p>
+    <button id='button' onClick={buttonClick}>click me</button>
+    {/* <button onClick={()=>setPrint(true)}>Click me</button> */}
     </div>
+    
   );
 }
